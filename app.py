9fea9eda05acf682856f53a919eb007a204fa99b
KiranK-25 <kk74.kumar6@gmail.com>
2023-12-30 17:49:01 +0530

    updated

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def home():
 @cross_origin()
 def trainRoute():
     os.system("python main.py")
-    # os.system("dvc repro")
+    os.system("dvc repro")
     return "Training done successfully!"
 
 
--- src/cnnClassifier/pipeline/Prediction.py
+++ src/cnnClassifier/pipeline/Prediction.py
@@ -3,8 +3,6 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
 
-
-
 class PredictionPipeline:
     def __init__(self,filename):
         self.filename =filename
@@ -12,7 +10,7 @@ class PredictionPipeline:
 
     def predict(self):
         # load model
-        model = load_model(os.path.join("model", "model.h5"))
+        model = load_model(os.path.join("models", "model.h5"))
 
         imagename = self.filename
         test_image = image.load_img(imagename, target_size = (224,224))
